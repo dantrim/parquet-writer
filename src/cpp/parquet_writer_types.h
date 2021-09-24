fafd671a1b4345551bd445d8b53ccb3c3bd8a326
dantrim <dantrim1023@gmail.com>
2021-09-24 08:04:46 -0700

    disable clang-format for variant definition

--- src/cpp/parquet_writer_types.h
+++ src/cpp/parquet_writer_types.h
@@ -8,17 +8,40 @@
 
 namespace parquetwriter {
 namespace types {
+// clang-format off
 typedef std::variant<
-    bool, uint8_t, uint16_t, uint32_t, uint64_t, int8_t, int16_t, int32_t,
-    int64_t, float, double, std::vector<bool>, std::vector<uint8_t>,
-    std::vector<uint16_t>, std::vector<uint32_t>, std::vector<uint64_t>,
-    std::vector<int8_t>, std::vector<int16_t>, std::vector<int32_t>,
-    std::vector<int64_t>, std::vector<float>, std::vector<double>,
-    std::vector<std::vector<bool>>, std::vector<std::vector<uint8_t>>,
-    std::vector<std::vector<uint16_t>>, std::vector<std::vector<uint32_t>>,
-    std::vector<std::vector<uint64_t>>, std::vector<std::vector<int8_t>>,
-    std::vector<std::vector<int16_t>>, std::vector<std::vector<int32_t>>,
-    std::vector<std::vector<int64_t>>, std::vector<std::vector<float>>,
+    bool,
+    uint8_t,
+    uint16_t,
+    uint32_t,
+    uint64_t,
+    int8_t,
+    int16_t,
+    int32_t,
+    int64_t,
+    float,
+    double,
+    std::vector<bool>,
+    std::vector<uint8_t>,
+    std::vector<uint16_t>,
+    std::vector<uint32_t>,
+    std::vector<uint64_t>,
+    std::vector<int8_t>,
+    std::vector<int16_t>,
+    std::vector<int32_t>,
+    std::vector<int64_t>,
+    std::vector<float>,
+    std::vector<double>,
+    std::vector<std::vector<bool>>,
+    std::vector<std::vector<uint8_t>>,
+    std::vector<std::vector<uint16_t>>,
+    std::vector<std::vector<uint32_t>>,
+    std::vector<std::vector<uint64_t>>,
+    std::vector<std::vector<int8_t>>,
+    std::vector<std::vector<int16_t>>,
+    std::vector<std::vector<int32_t>>,
+    std::vector<std::vector<int64_t>>,
+    std::vector<std::vector<float>>,
     std::vector<std::vector<double>>,
     std::vector<std::vector<std::vector<bool>>>,
     std::vector<std::vector<std::vector<uint8_t>>>,
@@ -40,6 +63,7 @@ typedef std::vector<std::vector<std::vector<buffer_value_t>>>
 typedef std::variant<buffer_value_t, buffer_value_vec_t, buffer_value_vec2d_t,
                      buffer_value_vec3d_t>
     buffer_t;
+// clang-format on
 };  // namespace types
 
 // convenience types to hide some internal ugly names
