e36d52c5f125931aa7e77a0f6f3950ba746d036f
dantrim <dantrim1023@gmail.com>
2021-10-01 12:03:46 -0700

    fix input dir in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ read_the_docs_build = os.environ.get("READTHEDOCS", None) == True
 #breathe_projects = {}
 
 #if read_the_docs_build:
-input_dir = '../parquet-writer'
+input_dir = '../src/cpp'
 output_dir = 'build'
 configureDoxyfile(input_dir, output_dir)
 subprocess.call('doxygen', shell=True)
