9037c249675565c63f74181c1dc14abae1298279
dantrim <dantrim1023@gmail.com>
2021-10-01 11:44:05 -0700

    update docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,28 @@
 import subprocess, os
 
+
+def configureDoxyfile(input_dir, output_dir):
+
+	with open('Doxyfile.in', 'r') as file :
+		filedata = file.read()
+
+	filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+	filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+	
+	with open('Doxyfile', 'w') as file:
+		file.write(filedata)
+
+# check if we're running on RTD servers
 read_the_docs_build = os.environ.get("READTHEDOCS", None) == True
+#if read_the_docs_build:
+#    subprocess.call("cd ../doxygen; doxygen", shell = True)
+breathe_projects = {}
 if read_the_docs_build:
-    subprocess.call("cd ../doxygen; doxygen", shell = True)
+	input_dir = '../parquet-writer'
+	output_dir = 'build'
+	configureDoxyfile(input_dir, output_dir)
+	subprocess.call('doxygen', shell=True)
+	breathe_projects['parquet-writer'] = output_dir + '/xml'
 
 # Configuration file for the Sphinx documentation builder.
 #
