933ba5f55a6c593cf86e10e30c2ef5d6193bacb8
dantrim <dantrim1023@gmail.com>
2021-10-01 11:59:10 -0700

    remove old doxyfil

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,20 @@
-import subprocess, os
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+import subprocess, os
 
 def configureDoxyfile(input_dir, output_dir):
 
@@ -24,23 +39,6 @@ if read_the_docs_build:
 	subprocess.call('doxygen', shell=True)
 	breathe_projects['parquet-writer'] = output_dir + '/xml'
 
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
 
 project = 'parquet-writer'
@@ -61,7 +59,6 @@ extensions = [
 ]
 
 # breathe configuration
-breathe_default_project = "parquet-writer"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -84,7 +81,8 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
-html_static_path = []
+html_static_path = ['_static']
 
 master_doc = 'index'
+
+breathe_default_project = "parquet-writer"
